e97f1d7583dc8e68d5c38196ebbfaded857cbf69
jernicozz <cendleader@gmail.com>
2020-02-11 15:38:37 +0300

    Initial Commit

---
+++ Dulang/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Dulang
+//
+//  Created by Дмитрий Маслюков on 11.02.2020.
+//  Copyright © 2020 Дмитрий Маслюков. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
